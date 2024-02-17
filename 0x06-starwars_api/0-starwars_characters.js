0a7c1606604f5a0ba1436e8a3fc51e6bbfe99476
Ardiy0011 <ardiyquarsh@gmail.com>
2024-02-17 00:46:01 -0800

    s-wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,18 +1,20 @@
 #!/usr/bin/node
+const util = require('util');
+const request = util.promisify(require('request'));
+const filmID = process.argv[2];
 
-const request = require('request');
+async function starwarsCharacters (filmId) {
+  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+  let response = await (await request(endpoint)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
 
-const url = 'https://swapi-api.hbtn.io/api/films/';
-request(`${url}` + process.argv[2], function (err, body) {
-  if (err) throw err;
-  const actorsto = JSON.parse(body).characters;
-  exactOrder(actorsto, 0);
-});
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
-  });
-};
+  for (let i = 0; i < characters.length; i++) {
+    const urlCharacter = characters[i];
+    let character = await (await request(urlCharacter)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
+  }
+}
+
+starwarsCharacters(filmID);
