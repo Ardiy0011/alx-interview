e8696cfaf86c76894f144cf717ca6f481b7c8ec9
Ardiy0011 <ardiyquarsh@gmail.com>
2024-02-17 00:42:08 -0800

    s-wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,14 +3,14 @@
 const request = require('request');
 
 const args = 'https://swapi-api.hbtn.io/api/films/';
-request(`${args}` + process.argv[2], function (err, body) {
+request(`${args}` + process.argv[2], function (err, res, body) {
   if (err) throw err;
-  const actorsto = JSON.parse(body).characters;
-  exactOrder(actorsto, 0);
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
 const exactOrder = (actors, x) => {
   if (x === actors.length) return;
-  request(actors[x], function (err, body) {
+  request(actors[x], function (err, res, body) {
     if (err) throw err;
     console.log(JSON.parse(body).name);
     exactOrder(actors, x + 1);
