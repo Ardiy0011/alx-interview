3e3d433f10735efb5a60bda42cfc5f68c7df7e3d
Ardiy0011 <ardiyquarsh@gmail.com>
2024-03-07 15:37:07 -0800

    removeing inbuilt test

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -21,14 +21,3 @@ def island_perimeter(grid):
                     perimeter -= 2
 
     return perimeter
-
-
-
-grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-print(island_perimeter(grid))
